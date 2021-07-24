d181c23a7c15e0ff9e0273e365749e2b2ce3813f
Navaneeth Reddy <navaneethreddydevops@gmail.com>
2021-07-24 13:33:13 -0500

    Adding vpc code

--- infra/main.tf
+++ infra/main.tf
@@ -14,7 +14,6 @@ provider "aws" {
 
 # Create a VPC
 resource "aws_vpc" "vpc_example" {
-  provider             = aws.region
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
