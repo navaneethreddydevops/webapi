8f853cea7b9d66f7e15b42db2e78f09919bcfd6c
Navaneeth Reddy <navaneethreddydevops@gmail.com>
2021-07-24 13:56:08 -0500

    Adding vpc subnets

--- infra/data.tf
+++ infra/data.tf
@@ -1,5 +1,1 @@
 # Data to get all availability zones of VPC for master region
-data "aws_availability_zones" "us-east-azs" {
-  provider = "aws.us-east-1"
-  state    = "available"
-}
--- infra/main.tf
+++ infra/main.tf
@@ -36,12 +36,12 @@ resource "aws_internet_gateway" "igw" {
 
 resource "aws_subnet" "subnet_1" {
   availability_zone = element(data.aws_availability_zones.us-east-azs.names, 0)
-  vpc_id            = aws_vpc.vpc_devops.id
+  vpc_id            = "us-east-1a"
   cidr_block        = "10.0.1.0/24"
 }
 
 resource "aws_subnet" "subnet_2" {
-  availability_zone = element(data.aws_availability_zones.us-east-azs.names, 1)
+  availability_zone = "us-east-1b"
   vpc_id            = aws_vpc.vpc_devops.id
   cidr_block        = "10.0.2.0/24"
 }
