888741c0bd9fbe06b1759d52e7abbc7810dfd2ae
Navaneeth Reddy <navaneethreddydevops@gmail.com>
2021-07-24 12:03:14 -0500

    s3 resource

--- infra/main.tf
+++ infra/main.tf
@@ -1,39 +1,9 @@
-
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "non-prod"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
 
   tags = {
-    Environment = "non-prod",
-    provisioned = "terraform"
-  }
-}
-
-module "ec2_cluster" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 2.0"
-
-  name           = "my-cluster"
-  instance_count = 5
-
-  ami                    = "ami-ebd02392"
-  instance_type          = "t2.micro"
-  key_name               = "user1"
-  monitoring             = true
-  vpc_security_group_ids = ["sg-12345678"]
-  subnet_id              = "subnet-eddcdzz4"
-
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
+    Name        = "My bucket"
+    Environment = "Dev"
   }
 }
