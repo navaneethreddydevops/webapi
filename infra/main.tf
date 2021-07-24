23051b8d74ed3e297d1d2ada1c16dd9bf59427e8
Navaneeth Reddy <navaneethreddydevops@gmail.com>
2021-07-24 12:59:09 -0500

    terraform apply

--- infra/main.tf
+++ infra/main.tf
@@ -1,13 +1,16 @@
 terraform {
   required_providers {
     aws = {
-      region  = "us-east-1"
       source  = "hashicorp/aws"
       version = "~> 3.0"
     }
   }
 }
 
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
 
 resource "aws_vpc" "vpc" {
   cidr_block           = "10.0.0.0/16"
