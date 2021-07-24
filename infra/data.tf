5231a4bd8df89631d0403178fd58f0fd87d38aea
Navaneeth Reddy <navaneethreddydevops@gmail.com>
2021-07-24 13:53:24 -0500

    Adding vpc subnets

--- infra/data.tf
+++ infra/data.tf
@@ -1,11 +1,5 @@
 # Data to get all availability zones of VPC for master region
 data "aws_availability_zones" "us-east-azs" {
-  provider = aws.region_master
-  state    = "available"
-}
-
-# Data to get all availability zones of VPC for worker region
-data "aws_availability_zones" "us-west-azs" {
-  provider = aws.region_worker
+  provider = "aws.us-east-1"
   state    = "available"
 }
