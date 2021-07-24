5bbef44814a6c4b4259e6349016216097bbc18ce
Navaneeth Reddy <navaneethreddydevops@gmail.com>
2021-07-24 13:27:48 -0500

    Adding vpc code

--- infra/main.tf
+++ infra/main.tf
@@ -18,6 +18,9 @@ resource "aws_vpc" "vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
+  tags = {
+    Name = "VPC"
+  }
 }
 
 resource "aws_internet_gateway" "igw" {
