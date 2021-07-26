b75c6125c361c04961782a435a1c6da7f88f3637
Navaneeth Reddy Alavala <navaneethreddydevops@gmail.com>
2021-07-25 22:23:52 -0500

    Update userdata.sh

--- infra/userdata.sh
+++ infra/userdata.sh
@@ -7,8 +7,7 @@ yum install -y yum-utils device-mapper-persistent-data lvm2 wget python3 wget ru
 pip3 install boto3 --user
 yum remove docker docker-common docker-selinux docker-engine-selinux docker-engine docker-ce -y
 yum install -y yum-utils device-mapper-persistent-data lvm2
-yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum install docker-ce -y
+yum install docker -y
 systemctl enable docker.service
 systemctl start docker
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
