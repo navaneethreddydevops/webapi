94b540a3b499b04265584ee1e12cbd9ed6a4a4f1
Navaneeth Reddy <navaneethreddydevops@gmail.com>
2021-07-25 22:33:52 -0500

    Adding the service docker start

--- infra/main.tf
+++ infra/main.tf
@@ -186,6 +186,12 @@ resource "aws_security_group" "instance_security_group" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = "5000"
+    to_port     = "5000"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
     from_port   = "80"
